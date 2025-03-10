607bc7155c45f2ead064d8af6331157c3b8a9855
Dilip raj <dr1117255@gmail.com>
2025-03-10 12:15:51 +0600

    first commit

---
+++ primitive/primitive.js
@@ -0,0 +1,13 @@
+// Primitive data type 
+// String = text 
+// Number = number
+// Boolean = true/false
+// undefined = a vaiable declared but not assinged ('let x')
+// Null = represents nothing  (let y = null)
+// BigInt = Large number (13421424234df)
+// Symbol = Unique identifier('Symbol(id)')
+
+// non-primitive data type
+// Object 
+// Array 
+// Function 
---
+++ text.js
@@ -0,0 +1,1 @@
+document.getElementById("someId").innerText = "Hello javaScript"
